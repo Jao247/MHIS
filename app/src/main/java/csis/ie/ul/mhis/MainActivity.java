ba420c05d422c374484b305dd4226aa1160faf5c
Jao247 <J@0247IsmYt4g>
2016-04-26 01:20:19 +0100

    Start of sword pages

--- app/src/main/java/csis/ie/ul/mhis/MainActivity.java
+++ app/src/main/java/csis/ie/ul/mhis/MainActivity.java
@@ -2,9 +2,8 @@ package csis.ie.ul.mhis;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if ( id == R.id.nav_sword)
         {
-            // Handle the camera action
+
         } else if ( id == R.id.nav_armour )
         {
 
