3394099b772308f21ab593f0e825c961e27a95c0
Jao247 <J@0247IsmYt4g>
2016-04-27 00:04:03 +0100

    Added placeholder data to the list

--- app/src/main/java/csis/ie/ul/mhis/activities/SwordList.java
+++ app/src/main/java/csis/ie/ul/mhis/activities/SwordList.java
@@ -1,5 +1,6 @@
 package csis.ie.ul.mhis.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -11,17 +12,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import csis.ie.ul.mhis.MainActivity;
 import csis.ie.ul.mhis.R;
 
 public class SwordList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -30,6 +33,36 @@ public class SwordList extends AppCompatActivity implements NavigationView.OnNav
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final ListView lView = (ListView) findViewById(R.id.sword_listView);
+        String[] listItems = {"Item 0", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10",
+                "Item 11", "Item 12", "Item 13", "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20"};
+
+        final ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < listItems.length; ++i) {
+            list.add(listItems[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        lView.setAdapter(adapter);
+
+        lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0).withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -37,6 +70,35 @@ public class SwordList extends AppCompatActivity implements NavigationView.OnNav
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String>
+    {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId, List<String> objects)
+        {
+            super(context, textViewResourceId, objects);
+            for ( int i = 0; i < objects.size(); ++i )
+            {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position)
+        {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds()
+        {
+            return true;
+        }
+
     }
 
     public void goHome(View view)
@@ -52,9 +114,9 @@ public class SwordList extends AppCompatActivity implements NavigationView.OnNav
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if ( id == R.id.nav_sword)
+        if ( id == R.id.nav_sword )
         {
-            Snackbar.make(getCurrentFocus(), "The world is in trouble.", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            Snackbar.make(getCurrentFocus(), R.string.sword_list_snackbar_msg, Snackbar.LENGTH_LONG).setAction("Action", null).show();
         } else if ( id == R.id.nav_armour )
         {
 
