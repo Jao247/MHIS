270c8aefe08086731fa03a86fa0748521f0b1dd4
adam115114 <adamdoherty1995@gmail.com>
2016-04-29 19:37:16 +0100

    FUCK Y'ALL

--- app/src/main/java/csis/ie/ul/mhis/bossDbHelper.java
+++ app/src/main/java/csis/ie/ul/mhis/bossDbHelper.java
@@ -1,6 +1,7 @@
 package csis.ie.ul.mhis;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -11,10 +12,15 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class bossDbHelper extends SQLiteOpenHelper{
 
-    private static final String DATABASE_NAME = "BOSSINFO.DB";
     private static final int DATABASE_VERSION = 1;
-    private static final String CREATE_QUERY =
-            "CREATE TABLE " + Boss_info.newbossInfo.BOSS_TABLE+ "(" + ")"; // CONTINUE WORK HERE
+    private static final String DATABASE_NAME = "Boss_info.db";
+    public static final String TABLE_PRODUCTS = "BossInfo";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_TYPE = "type";
+    public static final String COLUMN_WEAKNESS = "weakness";
+    public static final String COLUMN_ELEMENT = "element";
+
     public bossDbHelper(Context context){
 
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,11 +28,40 @@ public class bossDbHelper extends SQLiteOpenHelper{
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String query = "CREATE TABLE " + TABLE_PRODUCTS + "(" +
+                COLUMN_ID + " TEXT NOT NULL UNIQUE, " +
+                COLUMN_NAME + " TEXT NOT NULL UNIQUE, " +
+                COLUMN_TYPE + " TEXT NOT NULL, " +
+                COLUMN_WEAKNESS + " TEXT NOT NULL, " +
+                COLUMN_ELEMENT + " TEXT, " +
+                ");";
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
+    }
 
+    //print out the database as a string
+    public String databaseToString(){
+        String dbString = "";
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE 1";
+
+        //cursor points to a location in your results
+        Cursor c = db.rawQuery(query, null);
+        // move to the first row in you results
+        c.moveToFirst();
+
+        while (!c.isAfterLast()){
+            if(c.getString(c.getColumnIndex("productname")) != null){
+                dbString += c.getString(c.getColumnIndex("productname"));
+                dbString += "\n";
+            }
+        }
+        db.close();
+        return dbString;
     }
 }
