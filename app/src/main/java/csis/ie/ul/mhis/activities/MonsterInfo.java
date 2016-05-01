1bcd6e13b6dbcc4b4138d2cbfb150d98325b96f0
Jao247 <J@0247IsmYt4g>
2016-05-01 14:37:29 +0100

    Search Activity - fix to sword list

--- app/src/main/java/csis/ie/ul/mhis/activities/MonsterInfo.java
+++ app/src/main/java/csis/ie/ul/mhis/activities/MonsterInfo.java
@@ -6,21 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import com.squareup.picasso.Picasso;
+
 import csis.ie.ul.mhis.Data;
 import csis.ie.ul.mhis.R;
 
-/**
- * This outputs the information for a monster
- * @author Adam Doherty
- */
 public class MonsterInfo extends AppCompatActivity
 {
-    /**
-     * This method is called when the activity is called
-     * @param savedInstanceState This just needed
-     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -28,18 +22,14 @@ public class MonsterInfo extends AppCompatActivity
         setContentView(R.layout.activity_monster_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         Bundle dataIn = getIntent().getExtras();
         int pos = dataIn.getInt("id");
 
         changeDisplay(pos);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
-    /**
-     *
-     * @param pos
-     */
-
     public void changeDisplay(int pos){
         setTitle(Data.bossArray.get(pos).get_name());
 
@@ -48,12 +38,9 @@ public class MonsterInfo extends AppCompatActivity
         String imgURL = Data.bossArray.get(pos).get_url();
         try
         {
-            Toast.makeText(this, "Acquiring the image from the Wiki", Toast.LENGTH_LONG).show();
             Picasso.with(this).load(imgURL).placeholder(R.mipmap.ic_placeholder).into(iv);
         } catch (Exception e)
         {
-            Toast.makeText(this,"Failed to acquire image from the wiki", Toast.LENGTH_LONG).show();
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
 
